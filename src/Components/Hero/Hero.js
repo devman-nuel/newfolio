4e86af111202b345e3ac7aad897d32d9d265d507
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-08-30 01:28:28 +0100

    yes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,35 +1,88 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 import './Hero.css';
-import { Link } from 'react-router-dom';
 
 function Hero() {
+  const carouselRef = useRef(null);
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    let scrollAmount = 0;
+
+    const scrollCarousel = () => {
+      scrollAmount += 1;
+      if (scrollAmount >= carousel.scrollWidth - carousel.clientWidth) {
+        scrollAmount = 0; // Reset to the start
+      }
+      carousel.scrollLeft = scrollAmount;
+    };
+
+    const intervalId = setInterval(scrollCarousel, 20); // Adjust the speed by changing the interval
+
+    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+  }, []);
 
   return (
     <div className='hero'>
-      <h2>Digital Designer and Developer, I create unique web experiences and solve problems with code and design.</h2>
-
-      <div className='hero-img'>
-         <div className='line-about' >
-           <div className='hero-line'></div>
-           <Link to="/about" className='more-box'>About Me</Link>
-         </div>
-
-         <div>
-           <img className='hero-image' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716277829/17534_d5qgup.jpg' alt=''></img>
-         </div>
-
-         <div>
-           <img className='hero-image' src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716278034/zgc1993-Fk9RSVdJ6Q8-unsplash_j6yfxk.jpg' alt=''>
-         </img></div>
-      </div>
-
+      <h2>Digital Designer and Developer, I create unique web experiences and solve problems with design and code.</h2> 
       
-    
-      
+      <div className='carousel' ref={carouselRef}>
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220096/Frame_1686553134_r5gyza.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220095/Frame_1686553130_wkthtg.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+        
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220099/Frame_1686553131_ayuzxn.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220107/Frame_1686553126_chqri7.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220098/Frame_1686553117_zhlabl.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+
+        <div className='carousel-box'>
+          <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1724220096/Frame_1686553133_lrjpoo.jpg' alt='niche'></img>
+          <p>E-Commerce</p>
+          <div className='carousel-btn'>
+            <button>UI Design</button>
+            <button>Ux Design</button>
+          </div>
+        </div>
+
+      </div>  
     </div>
   )
 }
-
-
 
 export default Hero;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import  './Navbar.css'
 
 
 
 const Navbar = () => {
-    const [navOpen, setNavOpen] = useState(false)
-  
 
     return (
         <div className='nav'>
@@ -18,45 +14,9 @@ const Navbar = () => {
                     <Link to="/">DevMan_nuel</Link>
                 </div>
 
-                <div className='menu-toggle' onClick={() => setNavOpen(!navOpen)}>
-                    
-                    <div className= {navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
-                        <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
-                        <span className={navOpen ? "lineBottom spin" : "lineBottom"}></span>
-                    </div>
-                </div>
-            </div>
-
-            <div className='nav-overlay' style={{
-                left: navOpen ? "0" : "-100%",
-                transitionDelay : navOpen ? "0s" : "0s"
-            }}>
-
-                <ul className='nav-links'>
-                    <li className='nav-item'>
-                      <NavLink to="/" onClick={() => setNavOpen(!navOpen)} style={{
-                           top: navOpen ? "0" : "120px",
-                           transitionDelay : navOpen ? "0" : "0s"   
-                      }} >Home</NavLink>
-                       <div className='nav-wrapper'></div>
-                    </li>  
-
-                    <li className='nav-item'>
-                      <NavLink to="/about" onClick={() => setNavOpen(!navOpen)} style={{
-                         top: navOpen ? "0" : "120px",
-                         transitionDelay : navOpen ? "0s" : "2s" 
-                      }}>About</NavLink>
-                       <div className='nav-wrapper'></div>
-                    </li> 
-
-                    <li className='nav-item'>
-                      <NavLink to="/project" onClick={() => setNavOpen(!navOpen)} style={{
-                         top: navOpen ? "0" : "120px",
-                         transitionDelay : navOpen ? "0s" : "0s" 
-                      }}>Work</NavLink>
-                       <div className='nav-wrapper'></div>
-                    </li>                             
-                </ul>
+               <div className='nav-btn'>
+                <button>Hire Me</button>
+               </div>          
             </div>
            </div> 
         </div>       
